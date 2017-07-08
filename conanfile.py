27976ec1f377979846660ff913882be22c9cd449
Uilian Ries <uilianries@gmail.com>
2017-07-08 11:21:11 -0300

    Poco Lite
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,11 @@ class BeaglerelayConan(ConanFile):
     generators = "cmake"
     exports_sources = "src/*", "CMakeLists.txt"
     requires = "BeagleBoneBlackGPIO/0.2.0@uilianries/stable", "Poco/1.7.8p3@pocoproject/stable"
-    default_options = "BeagleBoneBlackGPIO:shared=True", "Poco:shared=True"
+    default_options = "BeagleBoneBlackGPIO:shared=True", "Poco:shared=True",
+    "Poco:enable_net=False", "Poco:enable_mongodb=False", "Poco:enable_data_sqlite=False"
+    "Poco:enable_netssl=False", "Poco:enable_netssl_win=False", "Poco:enable_util=False",
+    "Poco:enable_xml=False", "Poco:force_openssl=False", "Poco:enable_zip=False",
+    "Poco:enable_json=False", "Poco:enable_crypto=False", "Poco:enable_data=False"
 
     def build(self):
         cmake = CMake(self)
